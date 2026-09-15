2713b208eef5a8cac935777fead1ba498a4e26e5
zizhenliu0427 <liufeng0519@gmail.com>
2026-09-15 23:02:44 +1000

    fix: return 404/405 instead of 500 for unknown paths and wrong methods

--- stickybeak-common/src/main/java/au/com/stickybeak/common/exception/GlobalExceptionHandler.java
+++ stickybeak-common/src/main/java/au/com/stickybeak/common/exception/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package au.com.stickybeak.common.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import au.com.stickybeak.common.result.Result;
 import au.com.stickybeak.common.result.ResultCode;
@@ -31,6 +33,17 @@ public class GlobalExceptionHandler {
                 .findFirst()
                 .orElse(ResultCode.BAD_REQUEST.getMessage());
         return Result.fail(ResultCode.BAD_REQUEST.getCode(), msg);
+    }
+
+    /** Unknown path -> 404, wrong method -> 405 (must not fall through to 500). */
+    @ExceptionHandler(NoResourceFoundException.class)
+    public Result<Void> handleNotFound(NoResourceFoundException e) {
+        return Result.fail(ResultCode.NOT_FOUND.getCode(), "not found: " + e.getResourcePath());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Result<Void> handleMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        return Result.fail(405, "method not supported: " + e.getMethod());
     }
 
     @ExceptionHandler(Exception.class)
